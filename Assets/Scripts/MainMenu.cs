d7719aa795eadb8033a9033a6efb1090e61f5326
Charles <charles.dufour@epfl.ch>
2018-02-24 15:31:51 +0100

    test mais mauvaise qualité

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject gameButtonPrefab;
+    public GameObject gameButtonContainer;
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+
+    private void Start()
+    {
+
+        cameraTransform = Camera.main.transform;
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Games");
+        foreach(Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(gameButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(gameButtonContainer.transform, false);
+
+            string sceneName = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+
+        }
+    }
+
+    private void Update()
+    {
+        if (cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, 3 * Time.deltaTime);
+
+        }
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+           cameraDesiredLookAt = menuTransform;
+
+    }
+
+
+
+
+}
+
+
+
